5c891a1a1f47332b61e5253fb5463c860e55f261
MKIsHereOficial <mkishere.oficial@gmail.com>
2021-03-09 15:34:23 +0000

    Update 1.1:Initial Chatroom

--- src/App.js
+++ src/App.js
@@ -18,9 +18,44 @@ const firestore = firebase.firestore();
 const [user] = useAuthState(auth);
 
 function SignIn() {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+
+
     return (
         <button onClick={signInWithGoogle}>Entrar com o Google</button>
     )
+}
+function SignOut() {
+    return auth.currentUser && (
+        <button onClick={() => auth.signOut()}>Sair</button>
+    )
+}
+
+function ChatMessage(props) {
+    const {text, uid} = props.message;
+    
+    return <p>{text}</p>
+}
+
+function ChatRoom() {
+    const messagesRef = firestore.collection("messages");
+    const query = messagesRef.orderBy('createdAt').limit(25);
+
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+
+    return (
+        <div>
+            <div>
+                {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+            </div>
+            <div></div>
+        </div>
+    )
+
 }
 
 function App() {
